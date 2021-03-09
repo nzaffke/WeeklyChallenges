ff1c70c15f4edf86517be24171958821f470b02f
Nathan Zaffke <ekffaz@hotmail.com>
2021-03-09 10:47:38 -0600

    done with week 5

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -86,9 +86,15 @@ namespace ChallengesWithTestsMark8
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            for(int i = 0; i < words.Length; i++)
+            //for(int i = 0; i < words.Length; i++)
+            //{
+            //    words[i] = words[i].ToUpper();
+            //}
+            int x = 0;
+            foreach(string i in words)
             {
-                words[i] = words[i].ToUpper();
+                words[x] = words[x].ToUpper();
+                x++;
             }
         }
     }
--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,37 +8,104 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            return ((startNumber / n) + 1) * n;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < businesses.Length; i++)
+            {
+               if (businesses[i].TotalRevenue == 0)
+                {
+                    businesses[i].Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i-1])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            if(numbers == null)
+            {
+                return 0;
+            }
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if(numbers[i-1] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+            string sentence = "";
+            foreach(string word in words)
+            {
+                if(word.Trim().Length > 0)
+                {
+                    sentence += word.Trim() + " ";
+                }
+            }
+            if(sentence.Length == 0)
+            {
+                return "";
+            }
+            sentence = sentence.Substring(0, sentence.Length - 1);
+            sentence += ".";
+            return sentence;
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            double[] x = new double[0];
+            if(elements == null)
+            {
+                return x;
+            }
+            List<double> everyFourth = new List<double>();
+            for (int i = 3; i < elements.Count; i += 4)
+            {
+                everyFourth.Add(elements[i]);
+            }
+            return everyFourth.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int x = i + 1; x < nums.Length; x++)
+                {
+                    if(nums[i] + nums[x] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
